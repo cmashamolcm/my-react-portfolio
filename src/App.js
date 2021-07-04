cf633bee1ddb71e2a2c70414983a8b4ed8da3dc2
AshamolCM <cmashamolcm@gmail.com>
2021-07-04 23:18:22 +0530

    first commit for my portfolio website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/header/Header";
+import About from "./Components/about/About";
+import Resume from "./Components/resume/Resume";
+import Contact from "./Components/contact/Contact";
+import Service from "./Components/services/Service";
+import Footer from "./Components/footer/Footer";
+import Intro from "./Components/intro/Intro";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import $ from 'jquery'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      resumeData: {}
+    }
+  }
+
+   async getResumeData(){
+    const response =  await fetch('/resumeData.json',{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'cache': false,
+       }
+    });
+    if(response.ok){
+      const data = await response.json()
+      this.setState({resumeData: data});
+    }else{
+      alert('Unexpected Error');
+    }
+
+        
+
+  }
+
+  componentDidMount(){
+    window.$= $;
+    window.jQuery =$;
+    
+    this.getResumeData();
+    this.appendScript("js/plugins.js");
+    this.appendScript("js/main.js");
+  }
+
+  appendScript = (scriptToAppend) => {
+    const script = document.createElement("script");
+    script.src = scriptToAppend;
+    script.async = false;
+    document.body.appendChild(script);
+    }
+  
+  render(){
+    return (
+      <div className="App">
+        <Header></Header>
+        <Intro data={this.state.resumeData.intro}></Intro>
+        <About data={this.state.resumeData.about}></About>
+        <Resume data={this.state.resumeData.resume}></Resume>
+        <Service data={this.state.resumeData.service}></Service>
+        <Contact data={this.state.resumeData.contact}></Contact>
+        <Footer data={this.state.resumeData.footer}></Footer>
+      </div>
+    );
+  }
 }
-
-export default App;
